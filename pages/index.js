0930ee8ab47e282416df789af404799777606dad
Barnaby Bishop <bbishop@nine.com.au>
2021-02-22 22:27:19 +1100

    Most styling and page build, a bunch of content

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,228 @@
 import Head from 'next/head';
 
+const H2 = ({ children }) => (
+  <h2 className="text-3xl font-light">{children}</h2>
+);
+
+const Content = ({ classes, children }) => (
+  <div
+    className={`
+      h-auto
+      w-4/5
+      p-7
+      rounded
+      ml-5
+      -mt-4
+      shadow-heavy
+      space-y-3
+      ${classes}
+      `}
+  >
+    {children}
+  </div>
+);
+
+const Link = ({ href, classes, children }) => (
+  <a
+    href={href}
+    className={`underline hover:no-underline ${classes}`}
+    rel="noopener"
+    target="_blank"
+  >
+    {children}
+  </a>
+);
+
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Header stuff</title>
+        <title>Barnaby Bishop</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main
         className="
-          max-w-screen-lg
+          max-w-screen-md
           mx-auto
           bg-white
-          shadow-md
-          rounded-xl
           p-8
           m-8
           content-center
-          space-y-5"
+          space-y-4"
       >
-        <h1 className="text-5xl">Barnaby Bishop</h1>
-        <blockquote className="italic">
-          Engineering Manager building sustainable technology solutions
-        </blockquote>
-        <div>
-          Here is some text about a dude who does things, and sometimes, some{' '}
-          <em>stuff.</em>
+        <img
+          className="
+                border-solid
+                rounded-full 
+                border-4
+                border-black
+                float-right"
+          src="/profile.png"
+          alt="me"
+          width="128"
+          height="128"
+        />
+
+        <h1 className="text-5xl font-bold">Barnaby Bishop</h1>
+        <blockquote>Welcome to my very short website.</blockquote>
+
+        <div className="space-y-12">
+          <section>
+            <H2>About</H2>
+            <Content
+              classes="
+              bg-green-200
+              "
+            >
+              <p>
+                I'm an Engineering Manager who enjoys building cool things with
+                fun people.
+              </p>
+              <p>
+                It has been almost 20 years since I started working as a
+                software developer (yikes!) but recently I have been working
+                with NodeJS, ReactJS with deployment shenanigans in AWS.
+              </p>
+              <p>
+                I've been managing engineering teams for five years, it has been
+                fun.
+              </p>
+              <p>
+                Once upon a time I worked in .Net but that was a long time ago
+                in a galaxy far, far away.
+              </p>
+            </Content>
+          </section>
+
+          <section>
+            <H2>Handy Links</H2>
+            <Content classes="bg-blue-200">
+              <ul className="space-y-2">
+                <li>
+                  <Link
+                    href="https://github.com/BarnabyBishop/"
+                    classes="text-pink-500"
+                  >
+                    Github
+                  </Link>{' '}
+                  - come visit my land of unfinished projects.
+                </li>
+                <li>
+                  <Link
+                    href="https://www.linkedin.com/in/barnaby-bishop-4a209299/"
+                    classes="text-green-500"
+                  >
+                    Linked in
+                  </Link>{' '}
+                  - for business 💼
+                </li>
+                <li>
+                  <Link
+                    href="https://twitter.com/barneybishop"
+                    classes="text-indigo-500"
+                  >
+                    Twitter
+                  </Link>{' '}
+                  - I never post on twitter so no reason to follow me to be
+                  honest.
+                </li>
+                <li>
+                  <Link
+                    href="https://barnabybishop.com/2019"
+                    classes="text-gray-500"
+                  >
+                    2019
+                  </Link>{' '}
+                  - previous version of this site.
+                </li>
+              </ul>
+            </Content>
+          </section>
+
+          <section>
+            <H2>Personal Projects</H2>
+            <Content classes="bg-purple-200">
+              <ul className="space-y-2">
+                <li>
+                  <Link
+                    href="https://github.com/BarnabyBishop/bananapark"
+                    classes="text-pink-500"
+                  >
+                    bananapark.com.au
+                  </Link>{' '}
+                  - Static rendered e-commerce site built before it was cool!
+                  (GatsbyJS)
+                </li>
+                <li>
+                  <Link
+                    href="https://www.linkedin.com/in/barnaby-bishop-4a209299/"
+                    classes="text-green-500"
+                  >
+                    Linked in
+                  </Link>{' '}
+                  - for business 💼
+                </li>
+                <li>
+                  <Link
+                    href="https://twitter.com/barneybishop"
+                    classes="text-indigo-500"
+                  >
+                    Twitter
+                  </Link>{' '}
+                  - I never post on twitter so no reason to follow me to be
+                  honest.
+                </li>
+                <li>
+                  <Link
+                    href="https://barnabybishop.com/2019"
+                    classes="text-gray-500"
+                  >
+                    2019
+                  </Link>{' '}
+                  - previous version of this site.
+                </li>
+              </ul>
+            </Content>
+          </section>
+
+          <section>
+            <H2>About This Site</H2>
+            <Content
+              classes="
+              bg-red-200
+              space-y-3"
+            >
+              <p>
+                <span>Built with </span>
+                <Link href="https://nextjs.org/" classes="text-green-500">
+                  NextJS
+                </Link>
+                <span> and </span>
+                <Link href="https://nextjs.org/" classes="text-purple-500">
+                  Tailwind CSS
+                </Link>
+                .
+              </p>
+              <p>
+                NextJS is a tad overkill for this simple site but I wanted to
+                play with NextJS'&nbsp;
+                <Link
+                  href="https://nextjs.org/docs/basic-features/pages#static-generation-recommended"
+                  classes="text-blue-500"
+                >
+                  static generation
+                </Link>
+                . Plus I stopped any Javascript coming to the client for you
+                enjoyment.
+              </p>
+            </Content>
+          </section>
         </div>
       </main>
     </div>
